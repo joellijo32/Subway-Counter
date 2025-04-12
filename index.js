00d57558e369504a78abbdd21a2404a753c96e6c
Joel Lijo Mathew <joellijo4@gmail.com>
2025-04-12 22:42:31 +0530

    Completed a basic js project

---
+++ index.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+function increment() {
+  count++;
+  document.getElementById("count-el").innerText = count;
+}
+
+function decrement() {
+  if (count == 0) {
+    return;
+  }
+  count--;
+  document.getElementById("count-el").innerText = count;
+}
+
+function save() {
+  var saveEl = document.getElementById("save-el");
+  var result = saveEl.innerText;
+  result += " " + count + " - ";
+  saveEl.innerText = result;
+  count = 0;
+  document.getElementById("count-el").innerText = count;
+}
